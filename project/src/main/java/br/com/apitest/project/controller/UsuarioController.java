b57c1ce2d974528039fec0e44a5c8d80cd8b2d6d
Mateus Cristo <34775078+etmamate@users.noreply.github.com>
2023-08-15 16:11:03 -0300

    endpoint

--- project/src/main/java/br/com/apitest/project/controller/UsuarioController.java
+++ project/src/main/java/br/com/apitest/project/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package br.com.apitest.project.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class UsuarioController {
 
